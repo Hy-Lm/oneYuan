56cc2a58538c92bb1b69f4a61a6b318ecffb4489
song-xuebo <13663665247@163.com>
2021-06-11 18:19:44 +0800

    创建 particulars.vue

---
+++ components/particulars/particulars.vue
@@ -0,0 +1,28 @@
+<template>
+	<view>
+		<!-- 商品详情 公共页面-->
+		
+		
+		
+		
+		
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
